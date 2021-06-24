322412a18be5d1bfac48af50f0a6c09e4776bcfd
George <george.bungarzescu@gmail.com>
2021-06-24 14:04:06 +0300

    Create mirror-update.sh

---
+++ alpine/apk-addons/mirror-update.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#@todo update the code for busybox/sh
+
+config_file=<<<EOF
+
+mirror=http://tux.rainside.sk/alpine
+now=$(date +%Y-%m-%d-%H%M%S)
+repo=/etc/apk/repositories
+repoback=$repo-$now
+#version
+version=v3.13
+EOF
+### generate /etc/apk/config
+echo $config_file > /etc/apk/config
+
+############### use /etc/apk/config to generate /etc/apk/repositories ( or whatever is set as repo on config file
+########## @todo generate specific code
+########## @todo parametrize the call in order to override config file
+
+genmirror= <<<EOF 
+#!/bin/bash
+. /etc/apk/config
+cp $repo $repoback
+echo $mirror/$version/main > $repo
+echo $mirror/$version/community >> $repo
+cat $repo
+EOF
+
+echo $genmirror > /sbin/genapknmirror
+chmod a+x /sbin/genapkmirror
+#### usage : change required config options on /etc/apk/config and then call genapkmirror
+
+
+
+
