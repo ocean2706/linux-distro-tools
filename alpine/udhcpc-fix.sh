cd621e96d97b22b651012e8a86c2ed3a33f05e03
George <george.bungarzescu@gmail.com>
2021-08-16 10:11:31 +0300

    Update udhcpc-fix.sh

--- alpine/udhcpc-fix.sh
+++ alpine/udhcpc-fix.sh
@@ -1,1 +1,9 @@
-
+#!/bin/sh
+#### fix udhcp conf
+#### more explanation on https://wiki.alpinelinux.org/wiki/Udhcpc
+mkdir -p /etc/udhcpd/
+if [ -f /etc/udhcpd/udhcpd.conf ] then
+    echo "Config already exists"
+ else 
+  echo "dns=8.8.8.8 8.8.4.4" > /etc/udhcpd/udhcpd.conf
+ fi 
