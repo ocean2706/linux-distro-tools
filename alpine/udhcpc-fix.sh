145ac86f45f590d925bba823ebc4cde4386d1544
George <george.bungarzescu@gmail.com>
2021-08-16 12:28:23 +0300

    Update udhcpc-fix.sh

--- alpine/udhcpc-fix.sh
+++ alpine/udhcpc-fix.sh
@@ -6,5 +6,6 @@ mkdir -p /etc/udhcpc/
 if [ -f /etc/udhcpc/udhcpc.conf ] then
     echo "Config already exists"
  else 
+# set dns to google
   echo 'dns="8.8.8.8 8.8.4.4"' > /etc/udhcpc/udhcpc.conf
- fi 
+fi 
