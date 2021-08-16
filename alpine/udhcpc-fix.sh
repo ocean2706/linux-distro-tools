6ece6bc87f3fc21e0efcdf402d3303b7d0eddcc1
George <george.bungarzescu@gmail.com>
2021-08-16 10:17:14 +0300

    Update udhcpc-fix.sh

--- alpine/udhcpc-fix.sh
+++ alpine/udhcpc-fix.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 #### fix udhcp conf
-#### more explanation on https://wiki.alpinelinux.org/wiki/Udhcpc
-mkdir -p /etc/udhcpd/
-if [ -f /etc/udhcpd/udhcpd.conf ] then
+#### more explanation on https://wiki.alpinelinux.org/wiki/Udhcpc and
+### https://www.busybox.net/downloads/BusyBox.html
+mkdir -p /etc/udhcpc/
+if [ -f /etc/udhcpc/udhcpc.conf ] then
     echo "Config already exists"
  else 
-  echo "dns=8.8.8.8 8.8.4.4" > /etc/udhcpd/udhcpd.conf
+  echo "dns=8.8.8.8 8.8.4.4" > /etc/udhcpc/udhcpc.conf
  fi 
